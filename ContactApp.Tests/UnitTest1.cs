f49ea8c28fd2c3c6aa2e2438d656a85d1fdec1d9
Nsovo <manganyins@gmail.com>
2017-11-09 14:35:29 +0200

    add

--- ContactApp.Tests/UnitTest1.cs
+++ ContactApp.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ContactsApp.Models;
+using ContactsApp.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BookStoreApp.Tests
 {
@@ -6,9 +8,36 @@ namespace BookStoreApp.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanAddContact()
         {
+            var contact = new Contact();
+            contact.CellPhone = 0712544412;
+            contact.Email = "test@gmail.com";
+            contact.PhoneNumber = 011254441;
+            contact.UserId = 1;
 
+           var repository = new ContactRepository();
+            repository.Add(contact);
+            Assert.IsNull(contact);
+
+            //nothing much to test on a void
+        }
+
+        [TestMethod]
+        public void CanGetContacts()
+        {
+            var contact = new Contact();
+            contact.CellPhone = 0712544412;
+            contact.Email = "test@gmail.com";
+            contact.PhoneNumber = 011254441;
+            contact.UserId = 1;
+
+            var repository = new ContactRepository();
+            repository.Add(contact);
+
+            var contacts  = repository.GetContacts();
+            Assert.IsNull(contact);
+           // Assert.Equals(contacts);
         }
     }
 }
